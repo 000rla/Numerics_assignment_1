b0b4cd2548b91bf24ba98f2511c1229899d122da
000rla <133119584+000rla@users.noreply.github.com>
2024-10-21 16:11:50 +0100

    Updated main and added a main_animation version

--- run_me.py
+++ run_me.py
@@ -1,7 +1,19 @@
 import functions
 
 def main():
-    f=functions.solver
-    #f.finite_differences()
+    """runs the finite difference solvers for the linear advection equation
+    """
+    f=functions.solver()
+    f.FTBS()
+    f.FTCS()
+    f.CTCS()
+
+def main_animations():
+    """runs like main, but outputs animations of the solutions instead of a plot.
+    """
+    f=functions.solver(animation=True)
+    f.FTBS()
+    f.FTCS()
+    f.CTCS()
     
 main()
